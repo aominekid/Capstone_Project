e638dfa733b5e8b11008846ae4c7376f906b842f
aominekid <adkid>
2024-12-10 12:14:42 +0100

    some updates

--- main.tf
+++ main.tf
@@ -8,36 +8,48 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2" # Passe die Region an, falls nötig
+  region = "us-west-2"
 }
 
 # S3-Bucket erstellen
 resource "aws_s3_bucket" "event_photos" {
-  bucket = "event-photo-gallery-${random_string.suffix.result}" # Einzigartiger Name
-  acl    = "public-read"
-
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
-
+  bucket = "event-photo-gallery-${random_string.suffix.result}"
   tags = {
     Name = "EventPhotoGallery"
   }
 }
 
-# Zufälliger String für den Bucket-Namen (damit es einzigartig ist)
+# S3-Bucket ACL (statt direkt im Bucket zu setzen)
+resource "aws_s3_bucket_acl" "event_photos_acl" {
+  bucket = aws_s3_bucket.event_photos.id
+  acl    = "public-read"
+}
+
+# S3-Bucket Website Konfiguration
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.event_photos.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "404.html"
+  }
+}
+
+# Zufälliger String für den Bucket-Namen
 resource "random_string" "suffix" {
   length  = 6
   special = false
   upper   = false
 }
 
-# Outputs, um wichtige Informationen auszugeben
+# Outputs
 output "bucket_name" {
   value = aws_s3_bucket.event_photos.bucket
 }
 
 output "bucket_website_endpoint" {
-  value = aws_s3_bucket.event_photos.website_endpoint
+  value = aws_s3_bucket_website_configuration.website.website_endpoint
 }
